ebd217117cd4569974cc46553dfc1789fbf2bebc
fadrian06 <franyeradriansanchez@gmail.com>
2025-02-17 03:01:17 -0400

    apply dependency inversion principle

--- src/Controllers/Homepage.php
+++ src/Controllers/Homepage.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace NFT\Controllers;
 
+use Http\Response;
+
 final class Homepage
 {
-  static function show(): void
+  private $response;
+
+  function __construct(Response $response)
   {
-    echo 'Hello World';
+    $this->response = $response;
+  }
+
+  function show(): void
+  {
+    $this->response->setContent('Hello World');
   }
 }
