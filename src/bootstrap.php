6d4f24db0cd38a06f3afb47299c752f622f488a0
fadrian06 <franyeradriansanchez@gmail.com>
2025-02-17 02:34:29 -0400

    composer require filp/whoops

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,4 +2,32 @@
 
 declare(strict_types=1);
 
-echo 'Hello World!';
+namespace NFT;
+
+use Exception;
+use Throwable;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/**
+ * Register the error handler
+ */
+$whoops = new Run;
+
+if ($environment !== 'production') {
+  $whoops->pushHandler(new PrettyPageHandler);
+} else {
+  $whoops->pushHandler(static function (Throwable $error): void {
+    echo 'Todo: Friendly error page and send an email to the developer';
+  });
+}
+
+$whoops->register();
+
+throw new Exception;
